5bd66e51d61c56d2b9a031ecc91d48043520c970
Liam Diprose <liam.diprose@pbanz.com>
2020-07-14 15:23:03 +1200

    Add NixOS 20.03 image for Azure

--- nixos/modules/virtualisation/azure-images.nix
+++ nixos/modules/virtualisation/azure-images.nix
@@ -1,5 +1,7 @@
 let self = {
   "16.09" = "https://nixos.blob.core.windows.net/images/nixos-image-16.09.1694.019dcc3-x86_64-linux.vhd";
 
-  latest = self."16.09";
+  "20.03" = "https://nixopsstorage.blob.core.windows.net/nixops-7d5e0ebd-c480-11ea-b20d-0242ad3ebca8-vhds/azure-image.vhd";
+
+  latest = self."20.03";
 }; in self
